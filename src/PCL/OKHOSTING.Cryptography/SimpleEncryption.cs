d5b8dfd3d623c6c56c9590583f20c43de9712279
David Muñoz <david@okhosting.com>
2015-10-15 13:57:34 -0500

    Added test and password generation

--- src/PCL/OKHOSTING.Cryptography/SimpleEncryption.cs
+++ src/PCL/OKHOSTING.Cryptography/SimpleEncryption.cs
@@ -1,5 +1,7 @@
 ﻿using PCLCrypto;
+using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace OKHOSTING.Cryptography
@@ -69,6 +71,45 @@ namespace OKHOSTING.Cryptography
 		public static uint CreateRandomKey()
 		{
 			return WinRTCrypto.CryptographicBuffer.GenerateRandomNumber();
+		}
+
+        public static string CreateRandomPassword(int lenght, bool useNumbers, bool useUpperCase, bool useLowerCase, bool useSymbols)
+		{
+			var upperCase = new char[]
+				{
+				'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+				'V', 'W', 'X', 'Y', 'Z'
+				};
+
+			var lowerCase = new char[]
+				{
+				'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+				'v', 'w', 'x', 'y', 'z'
+				};
+
+			var numerals = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+			var symbols = new char[]
+				{
+				'~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '[', '}', ']', '-', '_', '=', '+', ':',
+				';', '|', '/', '?', ',', '<', '.', '>'
+				};
+
+			char[] total = (new char[0])
+							.Concat(useLowerCase ? upperCase : new char[0])
+							.Concat(useUpperCase ? lowerCase : new char[0])
+							.Concat(useNumbers ? numerals : new char[0])
+							.Concat(useSymbols ? symbols : new char[0])
+							.ToArray();
+
+			var rnd = new Random();
+
+			var chars = Enumerable
+				.Repeat<int>(0, lenght)
+				.Select(i => total[rnd.Next(total.Length)])
+				.ToArray();
+
+			return new string(chars);
 		}
 
 		/// <summary>
