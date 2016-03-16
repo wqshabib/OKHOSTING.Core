4eda8c1ef4ffc75bdf5cc80bfdd53a9db90116b3
David Muñoz <david@okhosting.com>
2016-03-16 17:44:14 -0600

    Nugets updated

---
+++ src/Xamarin/OKHOSTING.Core.Xamarin.Android/Class1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OKHOSTING.Core.Xamarin.Android
+{
+	public class Class1
+	{
+		public void Test()
+		{
+			
+		}
+	}
+}
---
+++ src/Xamarin/OKHOSTING.Core.Xamarin.Forms/Page.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace OKHOSTING.Core.Xamarin.Forms
+{
+	public class Page : ContentPage
+	{
+		public Page()
+		{
+			
+		}
+	}
+}
