f1fb38328c1cb31de0476144987529b9a711708e
Edgard David Iván Muñoz Chávez <david@okhosting.com>
2016-05-06 16:11:32 -0500

    moved htmltotext and texttohtml to pcl

--- src/PCL/OKHOSTING.Core/StringExtensions.cs
+++ src/PCL/OKHOSTING.Core/StringExtensions.cs
@@ -52,5 +52,53 @@ namespace OKHOSTING.Core
 			truncatedString += suffix;
 			return truncatedString;
 		}
-	}
+
+        /// <summary>
+        /// Formats a string to be correctly presented in a html page. Also replaces all system line breaks with html line breacks
+        /// </summary>
+        /// <param name="text">String to be formatted</param>
+        /// <returns>String correctly formatted for html presentation</returns>
+        public static string TextToHtml(this string text)
+        {
+            string html;
+
+            html = System.Net.WebUtility.HtmlEncode(text);
+            html = html.Replace("\n", "<br />");
+
+            return html;
+        }
+
+        /// <summary>
+        /// Creates a text only version of a html string, removing all html formatting and replacing html <br /> tags with new lines
+        /// </summary>
+        /// <param name="text">Html string to be converted to text</param>
+        /// <returns>Text version of the html string</returns>
+        public static string HtmlToText(this string html)
+        {
+            Regex regex;
+            string text = html;
+
+            //decode
+            text = System.Net.WebUtility.HtmlDecode(text);
+
+            //replace all <br />, </p> and </div> tags with new line chars
+            regex = new Regex("<br>", RegexOptions.IgnoreCase);
+            text = regex.Replace(text, "\n");
+
+            regex = new Regex("<br />", RegexOptions.IgnoreCase);
+            text = regex.Replace(text, "\n");
+
+            regex = new Regex("</p>", RegexOptions.IgnoreCase);
+            text = regex.Replace(text, "\n");
+
+            regex = new Regex("</div>", RegexOptions.IgnoreCase);
+            text = regex.Replace(text, "\n");
+
+            //remove the rest of html tags
+            regex = new Regex(RegexPatterns.HtmlTag);
+            text = regex.Replace(text, string.Empty);
+
+            return html;
+        }
+    }
 }
